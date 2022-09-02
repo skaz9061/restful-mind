bcf004a8acfeb5e094de02e8453eebe44bd38911
Steven Kazmierkiewicz <stevekaz@gmail.com>
2022-09-02 12:24:25 -0400

    initial structure built

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { SpecialtyComponent } from './specialty/specialty.component';
+import { InsuranceComponent } from './insurance/insurance.component';
+import { RatesComponent } from './rates/rates.component';
+import { BookingComponent } from './booking/booking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    AboutComponent,
+    SpecialtyComponent,
+    InsuranceComponent,
+    RatesComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
